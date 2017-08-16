d579c2a0da01d22dc8c14f45bfa377bcee1a605f
kofu <ko.fu.dev@gmail.com>
2017-08-17 01:06:33 +0900

    Implement adding request body

--- app/src/main/java/com/chronoscoper/android/netchecker/MainActivity.kt
+++ app/src/main/java/com/chronoscoper/android/netchecker/MainActivity.kt
@@ -17,13 +17,16 @@ package com.chronoscoper.android.netchecker
 
 import android.os.AsyncTask
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import kotterknife.bindView
 import okhttp3.*
@@ -39,12 +42,16 @@ class MainActivity : AppCompatActivity() {
     private val clearConsoleButton by bindView<Button>(R.id.clear_console)
     private val consoleView by bindView<TextView>(R.id.console)
 
+    private val inflater by lazy { LayoutInflater.from(this@MainActivity) }
+
+    private val recyclerAdapter by lazy { RequestBodyRecyclerAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         executeButton.click = View.OnClickListener {
-            executor.execute()
+            Executor().execute()
         }
 
         dumpCookieButton.click = View.OnClickListener {
@@ -60,10 +67,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         requestBodyListView.layoutManager = LinearLayoutManager(this)
-        requestBodyListView.adapter = RequestBodyRecyclerAdapter()
+        requestBodyListView.adapter = recyclerAdapter
 
         addRequestBodyButton.click = View.OnClickListener {
-            TODO()
+            val view = inflater.inflate(R.layout.add_body_dialog, null, false)
+            val dialog = AlertDialog.Builder(this).setView(view).show()
+            view.findViewById(R.id.done).click = View.OnClickListener {
+                requestBody.add(
+                        RequestBodyItem(
+                                (view.findViewById(R.id.name) as TextView).text.toString(),
+                                (view.findViewById(R.id.value) as TextView).text.toString(),
+                                (view.findViewById(R.id.encoded) as Switch).isChecked))
+                recyclerAdapter.notifyItemInserted(requestBody.size)
+                dialog.dismiss()
+            }
         }
     }
 
@@ -72,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         consoleView.append("\n")
     }
 
-    private val executor = object : AsyncTask<Unit, String, Unit>() {
+    inner class Executor : AsyncTask<Unit, String, Unit>() {
         private var url: String? = null
 
         override fun onPreExecute() {
@@ -125,7 +142,7 @@ class MainActivity : AppCompatActivity() {
         override fun onPostExecute(result: Unit?) {
             super.onPostExecute(result)
             executeButton.isEnabled = true
-            addRequestBodyButton.isEnabled = false
+            addRequestBodyButton.isEnabled = true
         }
     }
 
@@ -143,12 +160,26 @@ class MainActivity : AppCompatActivity() {
 
     inner class RequestBodyRecyclerAdapter : RecyclerView.Adapter<ViewHolder>() {
         override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+            val itemView = holder?.itemView
+            if (itemView != null) {
+                (itemView.findViewById(R.id.name) as TextView).text = requestBody[position].name
+                (itemView.findViewById(R.id.value) as TextView).text = requestBody[position].value
+                if (requestBody[position].encoded) {
+                    itemView.findViewById(R.id.encoded).visibility = View.VISIBLE
+                } else {
+                    itemView.findViewById(R.id.encoded).visibility = View.GONE
+                }
+                itemView.findViewById(R.id.delete).click = View.OnClickListener {
+                    requestBody.remove(requestBody[position])
+                    notifyItemRemoved(position)
+                }
+            }
         }
 
-        override fun getItemCount(): Int = requestBody.size + 1
+        override fun getItemCount(): Int = requestBody.size
 
         override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder =
-                ViewHolder(View(this@MainActivity))
+                ViewHolder(inflater.inflate(R.layout.request_body_item, parent, false))
 
     }
 
